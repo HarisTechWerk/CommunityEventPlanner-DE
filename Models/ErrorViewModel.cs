03d6ce81c10c4871e40e6808ac8fbf6e9e45a00b
Haris Qureshi <92890149+HarisTechWerk@users.noreply.github.com>
2025-01-15 12:10:00 +0100

    initial commit with setup Asp.Net Core MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CommunityEventPlanner_DE.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
